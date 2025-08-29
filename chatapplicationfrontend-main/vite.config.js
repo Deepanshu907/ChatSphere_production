0c41f060088c87a2c08736af752360baba01cb71
deepanshukumarjindal <deepanshujindal190@gmail.com>
2025-08-29 13:23:42 +0530

    update

--- chatapplicationfrontend-main/vite.config.js
+++ chatapplicationfrontend-main/vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',         // ⚡ Important for Netlify relative paths
+  // base: './',         // ⚡ Important for Netlify relative paths
   build: {
     outDir: 'dist',   // Default output folder
   }
